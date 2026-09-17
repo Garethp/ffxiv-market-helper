8aaae0b0febb7a72070a9342e3a75318d892f0fe
Gareth <blackfireweb@gmail.com>
2026-09-17 19:23:41 +0100

    Remove a stray console.log

--- src/containers/TrackedItemsContainer.tsx
+++ src/containers/TrackedItemsContainer.tsx
@@ -15,8 +15,6 @@ export const TrackedItemsContainer = ({
     currentCharacter,
   );
 
-  console.log(config);
-
   const { buyingRegions, params } = config;
   const sellWorld = currentCharacter?.homeWorld ?? "";
 
